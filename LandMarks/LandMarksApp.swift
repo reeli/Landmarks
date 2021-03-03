f1a6b4113314c4f2151cc8f2089fb90894e2f5b5
reeli <reeli.null@gmail.com>
2021-03-03 17:43:00 +0800

    Initial Commit

---
+++ LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by rrli  on 2021/3/3.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
